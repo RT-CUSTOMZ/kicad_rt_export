658a338bc946520d542477155c95884f6d940c2a
lars-2a <github@lnienhaus.de>
2023-03-24 00:01:28 +0100

    fixed edge_cut layer plotting issue

--- plot_pdf.py
+++ plot_pdf.py
@@ -2,8 +2,8 @@ import sys
 # from pcbnew import *
 import pcbnew
 
-if(pcbnew.Version()=="7.0.0"):
-    version7 = True
+majorVersion = int(pcbnew.Version().split(".")[0])
+
 
 def generatePdf(plotControl, plotOptions, ):
     # Set some important plot options:
@@ -14,12 +14,12 @@ def generatePdf(plotControl, plotOptions, ):
     plotOptions.SetScale(1)
     plotOptions.SetMirror(False)
     plotOptions.SetUseGerberAttributes(True)
-    if(not version7):
+    if (majorVersion < 7):
         plotOptions.SetExcludeEdgeLayer(False)
 
     plotOptions.SetScale(1)
     # plotOptions.SetUseAuxOrigin(True)
-    plotOptions.SetUseAuxOrigin(False) # drill file and pdf should be the same
+    plotOptions.SetUseAuxOrigin(False)  # drill file and pdf should be the same
 
     # This by gerbers only (also the name is truly horrid!)
     plotOptions.SetSubtractMaskFromSilk(False)
@@ -36,34 +36,34 @@ def generatePdf(plotControl, plotOptions, ):
     #### SilkTop.gbr     ####
     #########################
 
-    # Once the defaults are set it become pretty easy...
-    # I have a Turing-complete programming language here: I'll use it...
-    # param 0 is a string added to the file base name to identify the drawing
-    # param 1 is the layer ID
-    plot_plan = [
-        # ( "CuTop", F_Cu, "Top layer" ),
-        ( "CuBottom", pcbnew.B_Cu, "Bottom layer" ),
-        # ( "PasteBottom", B_Paste, "Paste Bottom" ),
-        # ( "PasteTop", F_Paste, "Paste top" ),
-        # ( "SilkTop", F_SilkS, "Silk top" ),
-        # ( "SilkBottom", B_SilkS, "Silk top" ),
-        # ( "MaskTop", F_Mask, "Mask top" ),
-        # ( "MaskBottom", B_Mask, "Mask bottom" ),
-        # ( "EdgeCuts", Edge_Cuts, "Edges" ),
-    ]
+    if (majorVersion >= 7):
+        # Plot Bottom
+        seq = pcbnew.LSEQ()
+        seq.push_back(pcbnew.Edge_Cuts)
+        seq.push_back(pcbnew.B_Cu)
 
-    if(version7):
-        layerselection = plotOptions.GetPlotOnAllLayersSelection()
-        addedlayerselection = layerselection.addLayer(pcbnew.Edge_Cuts)
-        plotOptions.SetPlotOnAllLayersSelection(addedlayerselection)
+        plotControl.OpenPlotfile(
+            "CuBottom", pcbnew.PLOT_FORMAT_PDF, "Bottom layer")
+        plotControl.PlotLayers(seq)
 
-    for layer_info in plot_plan:
-        plotControl.SetLayer(layer_info[1])
-        plotControl.OpenPlotfile(layer_info[0], pcbnew.PLOT_FORMAT_PDF, layer_info[2])
+        # Plot Top inverted
+        seq = pcbnew.LSEQ()
+        seq.push_back(pcbnew.Edge_Cuts)
+        seq.push_back(pcbnew.F_Cu)
+
+        plotOptions.SetMirror(True)
+        plotControl.OpenPlotfile("CuTop", pcbnew.PLOT_FORMAT_PDF, "Top layer")
+        plotControl.PlotLayers(seq)
+
+    else:
+        # plot Bottom
+        plotControl.SetLayer(pcbnew.B_Cu)
+        plotControl.OpenPlotfile(
+            "CuBottom", pcbnew.PLOT_FORMAT_PDF, "Bottom layer")
         plotControl.PlotLayer()
 
-    # plot Top inverted
-    plotOptions.SetMirror(True)
-    plotControl.SetLayer(pcbnew.F_Cu)
-    plotControl.OpenPlotfile("CuTop", pcbnew.PLOT_FORMAT_PDF, "Top layer")
-    plotControl.PlotLayer()
+        # plot Top inverted
+        plotOptions.SetMirror(True)
+        plotControl.SetLayer(pcbnew.F_Cu)
+        plotControl.OpenPlotfile("CuTop", pcbnew.PLOT_FORMAT_PDF, "Top layer")
+        plotControl.PlotLayer()
